e0dda36a78e304f96f1d7e54a63a94f573ba2276
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2025-07-28 21:45:45 +0530

    Store hashed passwords while creating users in DB and complete signin route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
 import express, { Request, Response, NextFunction } from "express";
 import { UserModel, ContentModel, TagModel, LinkModel } from "./db";
+import { Types } from "mongoose";
 import * as z from "zod";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 function inputCheck(req: Request, res: Response, next: NextFunction) {
   const username = req.body.username;
@@ -56,18 +63,42 @@ app.post("/api/v1/signup", inputCheck, async (req, res) => {
   if (user)
     return res.status(403).json("User already exists with this username");
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   try {
     await UserModel.create({
       username: username,
-      password: password,
+      password: hashedPassword,
     });
     res.status(200).json("Signed up");
   } catch (err) {
-    console.log(err);
+    res.status(500).json("Server Error : " + err);
   }
 });
 
-app.post("/api/v1/signin", inputCheck, (req, res) => {});
+app.post("/api/v1/signin", inputCheck, async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await UserModel.findOne({
+    username: username,
+  });
+
+  if (!user) return res.status(403).json("User does not exist");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) return res.status(403).json("Wrong email password");
+
+  const token = jwt.sign(
+    { id: (user._id as Types.ObjectId).toString() },
+    JWT_SECRET
+  );
+
+  res.status(200).json({
+    token: token,
+  });
+});
 
 app.post("/api/v1/content", (req, res) => {});
 
