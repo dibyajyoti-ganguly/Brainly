fccd84cb67846a76a0f5a225f17c4300b10c9525
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2025-07-28 14:57:23 +0530

    Perform initial setup using TypeScript and create the necessary DB Schemas

---
+++ src/db.ts
@@ -0,0 +1,62 @@
+require("dotenv").config();
+import mongoose, { Schema, Types, Document } from "mongoose";
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}`
+  )
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+interface IUser extends Document {
+  username: string;
+  password: string;
+}
+
+interface IContent extends Document {
+  link: string;
+  type: string;
+  title: string;
+  tags?: Types.ObjectId[];
+  userId: Types.ObjectId;
+}
+
+interface ITags extends Document {
+  title: string;
+}
+
+interface ILink {
+  hash: string;
+  userId: Types.ObjectId;
+}
+
+const contentTypes = ["image", "video", "article", "audio"] as const;
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const ContentSchema = new Schema<IContent>({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const TagSchema = new Schema<ITags>({
+  title: { type: String, required: true, unique: true },
+});
+
+const LinkSchema = new Schema<ILink>({
+  hash: { type: String, required: true, unique: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+const ContentModel = mongoose.model("Content", ContentSchema);
+const TagModel = mongoose.model("Tags", TagSchema);
+const LinkModel = mongoose.model("Link", LinkSchema);
+
+export { UserModel, ContentModel, TagModel, LinkModel };
